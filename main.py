e609e36d66a1fd7c25e092ac561dc3100127fbff
NightRyderrr <48996537+NightRyderrr@users.noreply.github.com>
2021-11-18 16:08:45 -0600

    Add files via upload

--- main.py
+++ main.py
@@ -1,42 +1,52 @@
 import keyboard
 import pyautogui
+import time
 
-#made by NightRyderrr
-#NightRyderrr8007 on discord
+# made by NightRyderrr
+# NightRyderrr8007 on discord
 
-
+global count
 file1 = open('numbers.txt', 'r')
 Lines = file1.readlines()
-#                            change if needed
-#detects if hotkey is pressed  |
-def getinput():               #\/
-    if keyboard.is_pressed('shift + ctrl'):
-        return True
-    
-
-#gets line from numbers.txt and writes them
-def getline():
-    count = 0
-    for line in Lines:
-        count += 1
-        pyautogui.write(line.format(count, line.strip()))
-        print(line.format(count, line.strip()))
-        time.sleep(1)
-        break
-
-        
-
-#main function
-#checks for input
-#if input == True , runs getline():
-#else: it passes
 while True:
-    getinput():
+    count = input("Type line to start one, if 1 leave blank or type 1")
+    if count == "" or " ":
+        count = 1
+        break
+    elif count != 1:
+        print("if you input text, close and reopen the script")
+        pass
+
+
+
+
+
+
+#                            change if needed
+# detects if hotkey is pressed  |
+def getinput():               # \/
+    if keyboard.is_pressed('insert'):
+        return True
+
+
+# gets line from numbers.txt and writes them
+def getline(count):
+    data = Lines[count]
+    pyautogui.write(data)
+    print(data)
+    time.sleep(1)
+
+
+
+# main function
+# checks for input
+# if input == True , runs getline():
+# else: it passes
+
+while True:
+    getinput()
     if getinput() == True:
-        getline():
+        getline(count)
+        count += 1
     else:
         pass
-         
-
-
-
